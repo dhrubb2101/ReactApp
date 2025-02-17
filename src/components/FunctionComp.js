a3acba007ede0c10650d5e402cb34ea649fa0940
dhrubb <dhrubbgupta@gmail.com>
2025-02-17 07:31:15 +0530

    Initial commit

---
+++ src/components/FunctionComp.js
@@ -0,0 +1,8 @@
+
+
+const FunctionComp = () => {   // to create a arrow function this is the syntax const FunctionComp = () => { //
+    //to create a normal function the syntax is function FunctionComp(){}
+    return <h2>This is function component</h2>
+}
+
+export default FunctionComp;
