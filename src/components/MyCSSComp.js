7a8147ee6d167801def0d3de583f58aff2831ff8
dhrubb <dhrubbgupta@gmail.com>
2025-02-22 02:21:31 +0530

    learned about to use css three different ways internal CSS, external CSS and Module CSS, module css is the most convenient and effective use

--- src/components/MyCSSComp.js
+++ src/components/MyCSSComp.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import './MyCSS.css';
+import moduleCss from './Hover.module.css'
 
 class MyCSSComp extends Component {
+
+    myBox={
+        border:"3px solid red",
+        width:"300px",
+        height:"200px",
+        backgroundColor:"pink"
+
+    }
+
+
+
     render (){
         return (<div>
              {/* use of internal css */}
              <h2 style={{color:"blue",textTransform:"uppercase" }}>This is my MyCSSComp component.</h2>
+             <div style={this.myBox}>Good Morning Friends!</div>
+
+             {/* use of external css */}
+             <p className="txt-danger">Welcome You ALL in my CSS Session</p>
+             <p className="txt-primary">My name is something that does not matter not right now anyway</p>
+             {/* use of module css */}
+             <div className={moduleCss.flipbox}>This is Opteamix </div>
+
         </div>)
     }
 }
